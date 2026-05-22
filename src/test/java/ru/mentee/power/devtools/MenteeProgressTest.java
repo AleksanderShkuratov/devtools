c1070089a74cc3ba414bf52ccb61bc7ea0f9fb22
Aleksander Shkuratov <shk888@yandex.ru>
2026-05-22 06:33:04 +0300

    DVT-6: Реализовать цикл подсчёта прогресса с Debug
     - Добавлен ProgressTracker.calculateProgress с циклом while
     - Реализованы тесты ProgressLoopTest (2 сценария)
     - Обновлён README со сценарием ручной проверки
     - Проверено через Debug: переменные изменяются корректно

--- src/test/java/ru/mentee/power/devtools/MenteeProgressTest.java
+++ src/test/java/ru/mentee/power/devtools/MenteeProgressTest.java
@@ -1,9 +1,8 @@
-package ru.mentee.power;
+package ru.mentee.power.devtools;
 
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class MenteeProgressTest {
 
